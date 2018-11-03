31e419b392d2cefe71bcd989c49a91ac2f71036c
Mostafa Farghaly <keepondev@gmail.com>
2018-11-03 22:03:52 +0400

    fetch blocks from the node

--- controller/blocks.js
+++ controller/blocks.js
@@ -1,11 +1,30 @@
 "use strict";
 
 const mockBlocks = require("../__mocks__/blocks");
+const Web3 = require('web3');
+//TODO: change provider url to environment variable
+const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 
 const getBlocks = (req, res, next) => {
-  res.send({
-    blocks: mockBlocks
-  });
+  web3.eth.getBlock("latest", function(err, block){
+    const number = block.number;
+    const batch = web3.createBatch();
+    const blocks = [];
+
+    for(var i = (number < 20 ? 0 : number - 20); i <= number ; i++){
+      batch.add(web3.eth.getBlock.request(i, function(err, block){
+        blocks.push(block);
+      }));
+    }
+
+    batch.add(web3.eth.getBlockNumber.request(function(){
+      res.send({
+        blocks: blocks
+      });
+    }));
+
+    batch.execute();
+  })
 };
 
 const getBlockByHash = (req, res, next) => {
