05a507fd201b282bb9274caa9f7d53479c777500
Mostafa Farghaly <keepondev@gmail.com>
2018-11-04 08:08:19 +0400

    pass node url as env variable

--- controller/blocks.js
+++ controller/blocks.js
@@ -3,7 +3,7 @@
 const mockBlocks = require("../__mocks__/blocks");
 const Web3 = require('web3');
 //TODO: change provider url to environment variable
-const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.NODE_URL))
 
 const getBlocks = (req, res, next) => {
   web3.eth.getBlock("latest", function(err, block){
