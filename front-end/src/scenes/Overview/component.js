58951d57ebec35010eec9796a126fcc263a5709c
Mostafa Farghaly <keepondev@gmail.com>
2018-11-13 17:02:33 +0400

    removed hard coded bootnode

--- front-end/src/scenes/Overview/component.js
+++ front-end/src/scenes/Overview/component.js
@@ -156,9 +156,7 @@ class Overview extends Component {
                 <strong>EnodeURL:</strong>
               </Typography>
               <Typography variant="body2">
-                <span id="enodeurl" style={{ wordBreak: 'break-word' }}>
-                  enode://a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c@52.16.188.185:30303
-                </span>
+                <span id="enodeurl" style={{ wordBreak: 'break-word' }} />
               </Typography>
               <Typography variant="body1">
                 <strong>JSON RPC Endpoint:</strong>
